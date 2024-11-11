6ee99b345155af0a74a988b4d864ae09dc5c6f81
AlWegner <alanwegnerm@gmail.com>
2024-11-11 16:40:42 -0300

    Recorriendo los botones

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 const carrito = document.querySelector('#carrito');
 const botones = document.querySelectorAll('.btn-primary');
 const template = document.querySelector('#template');
+
+botones.forEach((item) => {
+    console.log(item);
+});
