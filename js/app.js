151e16dc5c822667bfef9ae254c639fa14bf6a3e
AlWegner <alanwegnerm@gmail.com>
2024-11-11 16:45:01 -0300

    Creando funcion para agregar productos al carrito

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ const carrito = document.querySelector('#carrito');
 const botones = document.querySelectorAll('.btn-primary');
 const template = document.querySelector('#template');
 
+const agregarProductoAlCarrito = (e) => {
+    console.log('me diste click');
+};
+
 botones.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        console.log('Me diste click');
-    });
+    item.addEventListener('click', agregarProductoAlCarrito);
 });
