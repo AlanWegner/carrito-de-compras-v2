ab80a5f1eaa88f97b30a1c4d953b433294b0ed01
AlWegner <alanwegnerm@gmail.com>
2024-11-11 16:49:47 -0300

    Creando array donde almacenar los productos

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const carrito = document.querySelector('#carrito');
 const botones = document.querySelectorAll('.btn-primary');
 const template = document.querySelector('#template');
 
+const carritoArray = [];
+
 const agregarProductoAlCarrito = (e) => {
     console.log(e.target.dataset.fruta);
 };
