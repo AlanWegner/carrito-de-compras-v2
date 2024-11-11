8be7343b322a114ecc42b4608db1b59da6960eb4
AlWegner <alanwegnerm@gmail.com>
2024-11-11 16:46:05 -0300

    Probando su dataset

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const botones = document.querySelectorAll('.btn-primary');
 const template = document.querySelector('#template');
 
 const agregarProductoAlCarrito = (e) => {
-    console.log('me diste click');
+    console.log(e.target.dataset.fruta);
 };
 
 botones.forEach((item) => {
